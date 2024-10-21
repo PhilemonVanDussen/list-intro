1c52bc748138a8f1b297876c78791015a1138d78
PhilemonVanDussen <vandussenph709@northwested.org>
2024-10-21 13:45:41 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# Philemon VanDussen
+# 10/21/24
+# List Starter
+#3-1
+names = ['eathon', 'summer', 'kaylee', 'levi']
+print(names [0])
+print(names[1])
+print(names[2])
+print(names[3])
+#3-2
+print(f'{names [0]} likes to get icecream')
+print(f'{names[1]} likes eating chips and cookies')
+print(f'{names[2]} likes to play on her 3ds')
+print(f'Me and my friend {names[3]} likes to sit next to each other on the bus.')
+#3-3
+rides = ['E-bike', 'buick', 'volkswagon', 'bus' ]
+print(f'I like to ride on a {rides [0]} to get out of my house.')
+print(f'I drive my {rides [1]} to school on wednesdays and fridays.')
+print(f'Sometimes me and my dad take the {rides [2]} in the back roads so I can learn to drive a stickshift.')
+print(f'Taking the {rides [3]} to CTC is fun because I sit next to my friends.')
